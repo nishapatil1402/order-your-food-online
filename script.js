34e7c81840816a72ae0a0aaf0449c7c338e900fa
Nishigandha_Patil <nishigandha.patil@aretebrains.com>
2023-11-23 12:02:06 +0530

    Online food delivery

---
+++ script.js
@@ -0,0 +1,20 @@
+// function test() {
+//   console.log("this is a simple function");
+// }
+
+// test();
+
+// var test1 = () => {
+//   console.log("this is a Arrow function");
+// };
+// test1();
+
+function test() {
+  console.log("helloooooooo");
+
+  function demo() {
+    console.log("============");
+  }
+  demo();
+}
+test();
